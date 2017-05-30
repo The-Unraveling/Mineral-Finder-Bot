4ec0edd992d52f0d8ee3ea41ac8ae80659ef761e
Richard Nelson <ricky551@live.com>
2017-05-30 08:58:33 -0700

    MineralFinderBot.py updated to lower comment spam

--- MineralFinderBot.py
+++ MineralFinderBot.py
@@ -11,6 +11,19 @@ import time
 
 # WRITE CERTAIN INFO(BOT STARTING, BOT COMMENTING) TO A TXT FILE..
 
+def open_read_make(a):
+    if not os.path.isfile(a+".txt"):
+        print a+".txt not found, making file..."
+        comments_bot_made = []
+
+    else:
+        # read file into a list and remove any empsy values
+        with open("comments_bot_made.txt", "r") as f:
+            print "comments_bot_made.txt found, reading file:"
+            comments_bot_made = f.read()
+            comments_bot_made = comments_bot_made.split("\n")
+            comments_bot_made = list(filter(None, comments_bot_made))
+
 def main():
 
     print "Bot Starting..."
@@ -57,6 +70,18 @@ def main():
             comments_bot_made = comments_bot_made.split("\n")
             comments_bot_made = list(filter(None, comments_bot_made))
 
+    if not os.path.isfile("comments_bot_submissions.txt"):
+        print "comments_bot_submissions.txt not found, making file..."
+        comments_bot_submissions = []
+
+    else:
+        # read file into a list and remove any empsy values
+        with open("comments_bot_submissions.txt", "r") as f:
+            print "comments_bot_submissions.txt found, reading file:"
+            comments_bot_submissions = f.read()
+            comments_bot_submissions = comments_bot_submissions.split("\n")
+            comments_bot_submissions = list(filter(None, comments_bot_submissions))
+
     print "Begin monitoring comments..."
     print "Time: ", date.strftime(date_format)
     #for comment in subreddit.stream.comments():    #this line allows for indefinite running(DANGEROUS ON BIG SUBREDDITS)
@@ -66,166 +91,171 @@ def main():
         for comment in submission.comments.list():  #COMMENT LINE ABOVE, INDENT CODE BELOW
 
         # Maybe add submission filter here? and make this work with task scheduler
+            if submission.id not in comments_bot_submissions:
 
-            if comment.id not in comments_replied_to:
+                if comment.id not in comments_replied_to:
 
-                #print(submission.title)
-
-                i = 0
-
-                for i in range(0, len(mineral_names_on_wiki)):
-
-                    possible_mineral_match = re.search('\\b' + mineral_names_on_wiki[i] + '\\b', comment.body, re.IGNORECASE)
-                    # Search comments for mineral keywords
-
-                    if possible_mineral_match:
-
-                        break
-
-
-                if possible_mineral_match:
-                    possible_mineral_name = re.findall('\\b' + mineral_names_on_wiki[i] + '\\b', comment.body, re.IGNORECASE)
-
-                    for i in range(i+1,len(mineral_names_on_wiki)):
-                        possible_mineral_name_next = re.findall('\\b' + mineral_names_on_wiki[i] + '\\b',comment.body, re.IGNORECASE)
-                        if all(possible_mineral_name_next):
-
-                            if len(possible_mineral_name_next) > 0:
-
-                                possible_mineral_name.extend(possible_mineral_name_next)
-
-                    minerals_in_comment = []
+                    #print(submission.title)
 
                     i = 0
-                    while i < (len(possible_mineral_name)):
-                        minerals_in_comment.append(possible_mineral_name[i])
-                        i += 1
 
-                    author = comment.author
+                    for i in range(0, len(mineral_names_on_wiki)):
 
-                    if not re.match('MineralFinderBot|throwAwayBotToday',author.name):
+                        possible_mineral_match = re.search('\\b' + mineral_names_on_wiki[i] + '\\b', comment.body, re.IGNORECASE)
+                        # Search comments for mineral keywords
 
-                        comment_time = comment.created_utc
+                        if possible_mineral_match:
 
-                        current_time = time.time()
-
-                        time_diff = current_time - comment_time
-
-                        print time_diff, " seconds old"
-
-                        if time_diff < 3600: # if comment is less than an hour old, post reply
-
-                            print "\n"
-                            print "Bot replying to :", comment.id,
-                            print "author:", author.name
-                            print "\n", "Comment text: ", comment.body
-
-                            #"Talking about minerals? Maybe I can help!  " + "&nbsp;  " + "\n" \
-                            mineral_greeting = "These are the minerals I found within your comment:  "
-
-                            mineral_greeting_part2 = ""
-                            i = 0
-
-                            minerals_replying_to = []
-                            while i < len(minerals_in_comment):
-                                if not re.search(minerals_in_comment[i], mineral_greeting_part2, re.IGNORECASE):
-                                    mineral_greeting_part2 += (minerals_in_comment[i])
-                                    minerals_replying_to.append(minerals_in_comment[i])
-                                    if i < (len(minerals_in_comment) - 1):
-                                        mineral_greeting_part2 += ", "
-                                    else:
-                                        mineral_greeting_part2 += ".  " + "\n"
-                                i += 1
-
-                            mineral_greeting += mineral_greeting_part2.lower()
-
-                            mineral_greeting += "The links below can(hopefully) be used to help better" \
-                                                " determine what " \
-                                                "you're looking at.  \n"
-
-                            i = 0
-                            mineral_links = ""
-
-                            while i < len(minerals_replying_to):
-
-                                this_mineral = minerals_replying_to[i]
-
-                                mineral_links += "### " + minerals_replying_to[i].lower() + "  \n"\
-                                                 " [Wikipedia page for " + \
-                                                 minerals_replying_to[i].lower() + \
-                                            "]" + "("
-
-                                wiki_mineral = wikipedia.page(minerals_replying_to[i])
-
-                                mineral_links += wiki_mineral.url
-                                mineral_links += ")  "
-                                mineral_links += "  "
-                                mineral_links += "\n\n"
-
-                                wiki_image_list = wiki_mineral.images
-
-                                image_length = len(wiki_image_list)
-
-                                mineral_links += "Photos: |  " + "\n"\
-                                                 ":-------|  " + "\n"\
-                                    "[Link to Google images:] (" + "https://www.google.com/search?tbm=isch&q="\
-                                    + minerals_replying_to[i] + "+mineral) |  " + "\n"
-
-                                j =0; k = 1
-                                while j < image_length and k <= 3: # changes number of wiki images added
-
-                                    is_photo_mineral = re.search(minerals_replying_to[i],
-                                                        wiki_image_list[j], re.IGNORECASE)
-
-                                    if is_photo_mineral:
-                                        mineral_links += "[ Wikipedia Photo " + str(k) + " of " + \
-                                                minerals_replying_to[i].lower() + ".]"
-                                        mineral_links += "(" + wiki_image_list[j] + ") |  "\
-                                                        + "\n"
-
-                                        k += 1
-
-                                    j += 1
-
-                                mineral_links += "  "
-                                mineral_links += "&nbsp;"
-                                mineral_links += "  \n"
-
-                                i += 1
+                            break
 
 
-                            bot_beep_boop = "beep ^boop, I'm a bot! If you have any questions, " \
-                                            "or suggestions on how my programmer can make me better, " \
-                                            "send me a PM or add a suggestion to r/MineralFinderBot." \
-                                            "Comment will be deleted " \
-                                            "if score drops below -1."
+                    if possible_mineral_match:
+                        possible_mineral_name = re.findall('\\b' + mineral_names_on_wiki[i] + '\\b', comment.body, re.IGNORECASE)
 
-                            finished_mineral_reply = ""
-                            finished_mineral_reply += mineral_greeting
-                            finished_mineral_reply += mineral_links
-                            finished_mineral_reply += bot_beep_boop
+                        for i in range(i+1,len(mineral_names_on_wiki)):
+                            possible_mineral_name_next = re.findall('\\b' + mineral_names_on_wiki[i] + '\\b',comment.body, re.IGNORECASE)
+                            if all(possible_mineral_name_next):
+
+                                if len(possible_mineral_name_next) > 0:
+
+                                    possible_mineral_name.extend(possible_mineral_name_next)
+
+                        minerals_in_comment = []
+
+                        i = 0
+                        while i < (len(possible_mineral_name)):
+                            minerals_in_comment.append(possible_mineral_name[i])
+                            i += 1
+
+                        author = comment.author
+
+                        if not re.match('MineralFinderBot|throwAwayBotToday',author.name):
+
+                            comment_time = comment.created_utc
+
+                            current_time = time.time()
+
+                            time_diff = current_time - comment_time
+
+                            print time_diff, " seconds old"
+
+                            if time_diff < 3600: # if comment is less than an hour old, post reply
+
+                                print "\n"
+                                print "Bot replying to :", comment.id,
+                                print "author:", author.name
+                                print "\n", "Comment text: ", comment.body
+
+                                #"Talking about minerals? Maybe I can help!  " + "&nbsp;  " + "\n" \
+                                mineral_greeting = "These are the minerals I found within your comment:  "
+
+                                mineral_greeting_part2 = ""
+                                i = 0
+
+                                minerals_replying_to = []
+                                while i < len(minerals_in_comment):
+                                    if not re.search(minerals_in_comment[i], mineral_greeting_part2, re.IGNORECASE):
+                                        mineral_greeting_part2 += (minerals_in_comment[i])
+                                        minerals_replying_to.append(minerals_in_comment[i])
+                                        if i < (len(minerals_in_comment) - 1):
+                                            mineral_greeting_part2 += ", "
+                                        else:
+                                            mineral_greeting_part2 += ".  " + "\n"
+                                    i += 1
+
+                                mineral_greeting += mineral_greeting_part2.lower()
+
+                                mineral_greeting += "The links below can(hopefully) be used to help better" \
+                                                    " determine what " \
+                                                    "you're looking at.  \n"
+
+                                i = 0
+                                mineral_links = ""
+
+                                while i < len(minerals_replying_to):
+
+                                    this_mineral = minerals_replying_to[i]
+
+                                    mineral_links += "### " + minerals_replying_to[i].lower() + "  \n"\
+                                                     " [Wikipedia page for " + \
+                                                     minerals_replying_to[i].lower() + \
+                                                "]" + "("
+
+                                    wiki_mineral = wikipedia.page(minerals_replying_to[i])
+
+                                    mineral_links += wiki_mineral.url
+                                    mineral_links += ")  "
+                                    mineral_links += "  "
+                                    mineral_links += "\n\n"
+
+                                    wiki_image_list = wiki_mineral.images
+
+                                    image_length = len(wiki_image_list)
+
+                                    mineral_links += "Photos: |  " + "\n"\
+                                                     ":-------|  " + "\n"\
+                                        "[Link to Google images:] (" + "https://www.google.com/search?tbm=isch&q="\
+                                        + minerals_replying_to[i] + "+mineral) |  " + "\n"
+
+                                    j =0; k = 1
+                                    while j < image_length and k <= 3: # changes number of wiki images added
+
+                                        is_photo_mineral = re.search(minerals_replying_to[i],
+                                                            wiki_image_list[j], re.IGNORECASE)
+
+                                        if is_photo_mineral:
+                                            mineral_links += "[ Wikipedia Photo " + str(k) + " of " + \
+                                                    minerals_replying_to[i].lower() + ".]"
+                                            mineral_links += "(" + wiki_image_list[j] + ") |  "\
+                                                            + "\n"
+
+                                            k += 1
+
+                                        j += 1
+
+                                    mineral_links += "  "
+                                    mineral_links += "&nbsp;"
+                                    mineral_links += "  \n"
+
+                                    i += 1
 
 
-                            print finished_mineral_reply
-                            print "Posting comment..."
+                                bot_beep_boop = "beep ^boop, I'm a bot! If you have any questions, " \
+                                                "or suggestions on how my programmer can make me better, " \
+                                                "send me a PM or add a suggestion to r/MineralFinderBot." \
+                                                "Comment will be deleted " \
+                                                "if score drops below -1."
 
-                            comment.reply(finished_mineral_reply)
-                            with open("comments_replied_to.txt","a") as myfile:
-                                myfile.write(comment.id + "\n")
-                            comments_replied_to.append(comment.id)
+                                finished_mineral_reply = ""
+                                finished_mineral_reply += mineral_greeting
+                                finished_mineral_reply += mineral_links
+                                finished_mineral_reply += bot_beep_boop
 
-                            date = datetime.datetime.now()
-                            print "Comment posted at " + date.strftime(date_format)
 
-                            print "Bot has now replied to "
-                            print len(comments_replied_to)
-                            print "comments!"
+                                print finished_mineral_reply
+                                print "Posting comment..."
 
-                    else:
-                        with open("comments_bot_made.txt", "a") as myfile:
-                            if comment.id not in comments_bot_made:
-                                myfile.write(comment.id + "\n")
-                        comments_bot_made.append(comment.id)
+                                comment.reply(finished_mineral_reply)
+                                with open("comments_replied_to.txt","a") as myfile:
+                                    myfile.write(comment.id + "\n")
+                                comments_replied_to.append(comment.id)
+
+                                with open("comments_bot_submissions.txt", "a") as f:
+                                    f.write(submission.id + "\n")
+                                comments_bot_submissions.append(submission.id)
+
+                                date = datetime.datetime.now()
+                                print "Comment posted at " + date.strftime(date_format)
+
+                                print "Bot has now replied to "
+                                print len(comments_replied_to)
+                                print "comments!"
+
+                        else:
+                            with open("comments_bot_made.txt", "a") as myfile:
+                                if comment.id not in comments_bot_made:
+                                    myfile.write(comment.id + "\n")
+                            comments_bot_made.append(comment.id)
 
 
 
@@ -235,5 +265,7 @@ def main():
     print len(comments_replied_to)
     print "comments"
 
+
+#open_read("mineral_names_on_wiki")
 
 main()
